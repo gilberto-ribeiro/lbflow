2b39d7549ff368fbcda65f6f95cefbe1ca0e5e33
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2025-08-26 04:19:33 -0300

    Adaptação para MRT e adição de getters e setters para VelocitySetParameters

--- src/velocity_set.rs
+++ src/velocity_set.rs
@@ -11,7 +11,7 @@ use crate::prelude::*;
 use crate::{FACES_2D, FACES_3D};
 use std::collections::HashMap;
 
-pub type VelocityComputation = fn(Float, Vec<Float>) -> Vec<Float>;
+pub type VectorComputation = fn(Float, Vec<Float>) -> Vec<Float>;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum VelocitySet {
@@ -43,6 +43,14 @@ impl VelocitySet {
                     (North, 4),
                 ]),
                 velocity_computation: Some(d2q9::velocity_computation),
+                mrt_matrix: d2q9::MRT_MATRIX.iter().map(|row| row.to_vec()).collect(),
+                mrt_inverse_matrix: d2q9::MRT_INVERSE_MATRIX
+                    .iter()
+                    .map(|row| row.to_vec())
+                    .collect(),
+                mrt_equilibrium_moments_computation: Some(
+                    d2q9::mrt_equilibrium_moments_computation,
+                ),
             },
             D3Q15 => VelocitySetParameters {
                 velocity_set: D3Q15,
@@ -65,6 +73,14 @@ impl VelocitySet {
                     (Top, 6),
                 ]),
                 velocity_computation: None,
+                mrt_matrix: d3q15::MRT_MATRIX.iter().map(|row| row.to_vec()).collect(),
+                mrt_inverse_matrix: d3q15::MRT_INVERSE_MATRIX
+                    .iter()
+                    .map(|row| row.to_vec())
+                    .collect(),
+                mrt_equilibrium_moments_computation: Some(
+                    d3q15::mrt_equilibrium_moments_computation,
+                ),
             },
             D3Q19 => VelocitySetParameters {
                 velocity_set: D3Q19,
@@ -87,6 +103,14 @@ impl VelocitySet {
                     (Top, 6),
                 ]),
                 velocity_computation: None,
+                mrt_matrix: d3q19::MRT_MATRIX.iter().map(|row| row.to_vec()).collect(),
+                mrt_inverse_matrix: d3q19::MRT_INVERSE_MATRIX
+                    .iter()
+                    .map(|row| row.to_vec())
+                    .collect(),
+                mrt_equilibrium_moments_computation: Some(
+                    d3q19::mrt_equilibrium_moments_computation,
+                ),
             },
             D3Q27 => VelocitySetParameters {
                 velocity_set: D3Q27,
@@ -109,6 +133,11 @@ impl VelocitySet {
                     (Top, 6),
                 ]),
                 velocity_computation: Some(d3q27::velocity_computation),
+                mrt_matrix: d3q27::MRT_MATRIX.iter().map(|row| row.to_vec()).collect(),
+                mrt_inverse_matrix: compute_inverse_matrix(
+                    d3q27::MRT_MATRIX.iter().map(|row| row.to_vec()).collect(),
+                ),
+                mrt_equilibrium_moments_computation: None,
             },
         }
     }
@@ -124,7 +153,10 @@ pub struct VelocitySetParameters {
     pub q_bar: Vec<usize>,
     pub q_faces: HashMap<BoundaryFace, Vec<usize>>,
     pub face_normal_directions: HashMap<BoundaryFace, usize>,
-    pub velocity_computation: Option<VelocityComputation>,
+    pub velocity_computation: Option<VectorComputation>,
+    pub mrt_matrix: Vec<Vec<Float>>,
+    pub mrt_inverse_matrix: Vec<Vec<Float>>,
+    pub mrt_equilibrium_moments_computation: Option<VectorComputation>,
 }
 
 impl Default for VelocitySetParameters {
@@ -160,6 +192,75 @@ impl VelocitySetParameters {
             2 => Default::default(),
             3 => D3Q27.get_velocity_set_parameters(),
             _ => panic!("Unsupported dimension: {dim}"),
+        }
+    }
+}
+
+impl VelocitySetParameters {
+    pub fn get_d(&self) -> usize {
+        self.d
+    }
+
+    pub fn get_q(&self) -> usize {
+        self.q
+    }
+
+    pub fn get_c(&self) -> &Vec<Vec<i32>> {
+        &self.c
+    }
+
+    pub fn get_w(&self) -> &Vec<Float> {
+        &self.w
+    }
+
+    pub fn get_q_bar(&self) -> &Vec<usize> {
+        &self.q_bar
+    }
+
+    pub fn get_q_faces(&self, boundary_face: &BoundaryFace) -> &Vec<usize> {
+        self.q_faces
+            .get(boundary_face)
+            .expect("Boundary face not found")
+    }
+
+    pub fn get_face_normal_direction(&self, boundary_face: &BoundaryFace) -> &usize {
+        self.face_normal_directions
+            .get(boundary_face)
+            .expect("Boundary face not found")
+    }
+
+    pub fn get_velocity_computation(&self) -> Option<VectorComputation> {
+        self.velocity_computation
+    }
+
+    pub fn get_opposite_direction(&self, direction: usize) -> usize {
+        self.get_q_bar()[direction]
+    }
+
+    pub fn get_mrt_matrix(&self) -> &Vec<Vec<Float>> {
+        &self.mrt_matrix
+    }
+
+    pub fn get_mrt_inverse_matrix(&self) -> &Vec<Vec<Float>> {
+        &self.mrt_inverse_matrix
+    }
+
+    pub fn get_mrt_equilibrium_moments_computation(&self) -> Option<&VectorComputation> {
+        self.mrt_equilibrium_moments_computation.as_ref()
+    }
+}
+
+fn compute_inverse_matrix(matrix: Vec<Vec<Float>>) -> Vec<Vec<Float>> {
+    let rows = matrix.len();
+    let cols = matrix[0].len();
+    let flat_data: Vec<f64> = matrix.iter().flatten().cloned().collect();
+    let dmatrix = nalgebra::DMatrix::from_row_slice(rows, cols, &flat_data);
+    match dmatrix.try_inverse() {
+        Some(inverse) => (0..inverse.nrows())
+            .map(|i| inverse.row(i).iter().cloned().collect())
+            .collect::<Vec<Vec<f64>>>(),
+        None => {
+            panic!("Singular matrix!")
         }
     }
 }
